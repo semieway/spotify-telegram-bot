d9d8131ec9ce922b8e8f5f64a895971e42fb588a
Alex <semieway@gmail.com>
2020-12-01 21:22:47 +0500

    Add vendor directory to gitignore

--- src/index.php
+++ src/index.php
@@ -4,7 +4,6 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use TelegramBot\Api\BotApi;
 use TelegramBot\Api\Types\Message;
-use TelegramBot\Api\Types\ReplyKeyboardMarkup;
 use TelegramBot\Api\Types\Update;
 use TelegramBot\Api\Client;
 
@@ -14,25 +13,24 @@ $spotifyToken = getenv('SPOTIFY_TOKEN');
 $bot = new botApi($telegramToken);
 $client = new Client($telegramToken);
 
-$client->command('start', function (Message $message, $code) use ($bot) {
+$client->command('getrandomtopsong', function (Message $message) use ($bot) {
+    $bot->sendMessage(
+        $message->getChat()->getId(),
+        'Some text'
+    );
+});
+
+$client->command('start', function (Message $message) use ($bot) {
    $bot->sendMessage(
        $message->getChat()->getId(),
-       'Welcome to Spotify Music Bot!',
-       'html',
-       false,
-       null,
-       new ReplyKeyboardMarkup(
-           [
-               ['Get a song from Global Top'],
-           ]
-       )
+       'Welcome to Spotify Music bot! Use /getrandomtopsong, for example'
    );
 });
 
 $client->on(function (Update $update) use ($bot) {
-   $message = $update->getMessage();
-   $id = $message->getChat()->getId();
-   $bot->sendMessage($id, 'Your message: ' . $message->getText());
+    $message = $update->getMessage();
+    $id = $message->getChat()->getId();
+    $bot->sendMessage($id, 'Please try to use commands, for example /getrandomtopsong');
 }, function () {
     return true;
 });
