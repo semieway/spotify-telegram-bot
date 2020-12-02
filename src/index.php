b741e0a041406bb4018a1a8c576d2078c8b35123
Alex <semieway@gmail.com>
2020-12-02 14:47:26 +0500

    Implement recommend command

--- src/index.php
+++ src/index.php
@@ -22,50 +22,83 @@ $spotify->requestCredentialsToken();
 $spotifyApi = new SpotifyWebAPI();
 $spotifyApi->setAccessToken($spotify->getAccessToken());
 
-/* /top %country% command */
-$client->command('top', function (Message $message) use ($bot, $spotifyApi) {
-    $playlist = $spotifyApi->search($message->getText() . ' top 50 chart', 'playlist', ['limit' => 1]);
-    $playlistId = $playlist->playlists->items[0]->id;
-    $songs = $spotifyApi->getPlaylistTracks($playlistId);
-
-    $result = sprintf("<b>%s:</b>\n\n", $playlist->playlists->items[0]->name);
-    foreach ($songs->items as $key => $item) {
-        $artists = array_map(fn($artist) => $artist->name, $item->track->artists);
-        $artists = join(', ', $artists);
-        $temp = sprintf("<a href=\"%s\"><b>%d.</b> %s – %s</a>\n", $item->track->external_urls->spotify, $key + 1, $artists, $item->track->name);
-        $result .= $temp;
-    }
-
-    $bot->sendMessage(
-        $message->getChat()->getId(),
-        $result,
-        'HTML',
-        true
-    );
-});
-
-/* /start command */
-$client->command('start', function (Message $message) use ($bot) {
-   $bot->sendMessage(
-       $message->getChat()->getId(),
-       'Welcome to Spotify Music bot! Use /getrandomtopsong, for example'
-   );
-});
-
-/* Any messages other than commands */
-$client->on(function (Update $update) use ($bot) {
-    $message = $update->getMessage();
-    $id = $message->getChat()->getId();
-    $bot->sendMessage($id, 'Please try to use commands, for example /getrandomtopsong');
-}, function () {
-    return true;
-});
-
-/* Send response to bot */
 try {
-    $client->run();
-} catch (\TelegramBot\Api\InvalidJsonException $e) {
+    /* /top %country% command */
+    $client->command('top', function (Message $message) use ($bot, $spotifyApi) {
+        $playlist = $spotifyApi->search(substr($message->getText(), 5) . ' top 50 chart', 'playlist', ['limit' => 1]);
+        $playlistId = $playlist->playlists->items[0]->id;
+        $songs = $spotifyApi->getPlaylistTracks($playlistId);
+
+        $result = sprintf("<b>%s:</b>\n\n", $playlist->playlists->items[0]->name);
+        foreach ($songs->items as $key => $item) {
+            $artists = array_map(fn($artist) => $artist->name, $item->track->artists);
+            $artists = join(', ', $artists);
+            $temp = sprintf("<a href=\"%s\"><b>%d.</b> %s – %s</a>\n", $item->track->external_urls->spotify, $key + 1, $artists, $item->track->name);
+            $result .= $temp;
+        }
+
+        $bot->sendMessage(
+            $message->getChat()->getId(),
+            $result,
+            'HTML',
+            true
+        );
+    });
+
+    /* /recommend %song name% command */
+    $client->command('recommend', function (Message $message) use ($bot, $spotifyApi) {
+        $track = $spotifyApi->search(substr($message->getText(), 11), 'track', ['limit' => 1]);
+        $artists = array_map(fn($artist) => $artist->name, $track->tracks->items[0]->artists);
+        $artists = join(', ', $artists);
+        $result = sprintf("Recommendations for <b>«%s – %s»</b>:\n", $artists, $track->tracks->items[0]->name);
+
+        if ($trackId = $track->tracks->items[0]->id) {
+            $artistId = $track->tracks->items[0]->artists[0]->id;
+            $recommends = $spotifyApi->getRecommendations([
+                'limit' => 5,
+                'seed_artists' => [$artistId],
+                'seed_tracks' => [$trackId],
+            ]);
+
+            foreach ($recommends->tracks as $track) {
+                $artists = array_map(fn($artist) => $artist->name, $track->artists);
+                $artists = join(', ', $artists);
+                $temp = sprintf("<a href=\"%s\">• %s – %s</a>\n", $track->external_urls->spotify, $artists, $track->name);
+                $result .= $temp;
+            }
+        } else {
+            $result .= 'Sorry, nothing was found';
+        }
+
+        $bot->sendMessage(
+            $message->getChat()->getId(),
+            $result,
+            'HTML',
+            true
+        );
+    });
+
+    /* /start command */
+    $client->command('start', function (Message $message) use ($bot) {
+        $bot->sendMessage(
+            $message->getChat()->getId(),
+            'Welcome to Spotify Music bot! Use /getrandomtopsong, for example'
+        );
+    });
+
+    /* Any messages other than commands */
     $client->on(function (Update $update) use ($bot) {
-       $bot->sendMessage($update->getMessage()->getChat()->getId(), 'Sorry, this bot currently unavailable');
+        $message = $update->getMessage();
+        $id = $message->getChat()->getId();
+        $bot->sendMessage($id, 'Please try to use commands, for example /getrandomtopsong');
+    }, function () {
+        return true;
+    });
+
+    /* Send response to bot */
+    $client->run();
+} catch (\Throwable $e) {
+    $client->on(function (Update $update) use ($bot) {
+        $bot->sendMessage($update->getMessage()->getChat()->getId(), 'Sorry, something went wrong');
     });
 }
